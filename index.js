85aa7f1a8e5e737b50851e5a7e36024a466ec72e
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-06-10 13:54:29 +0300

    commit tree e7e531fae846fa8e260194145cb51eeee547e5c5

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+//soln here
+function distanceFromHqInBlocks(distnc) {
+  if (distnc > 42 ) {
+    return distnc - 42;
+  } else {
+    return 42 - distnc
+  } 
+}
+
+function distanceFromHqInFeet(distnc) {
+  return distanceFromHqInBlocks(distnc)*264 ;
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+if ( start < destination){
+  return (destination - start )* 264; 
+}
+else {
+  return (start-destination)* 264;
+}
+}
+function calculatesFarePrice(start, destination)  {
+  const distnc= distanceTravelledInFeet(start, destination)
+  if (distnc < 400) {
+    return 0;
+  }
+  else if ( distnc > 400 && distnc < 2000) {
+    return 0.02 * (distnc - 400 );
+  }
+  else if ( distnc > 2000 && distnc < 2500 ){
+    return 25;
+  }
+  else { 
+  return  'cannot travel that far';
+
+}
+
+
+}
+
